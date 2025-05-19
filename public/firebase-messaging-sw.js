dcc1038f4d29a961e2a60cf89aa620e02ae96f93
Michel Martins <michelmartins9164@gmail.com>
2025-05-18 21:08:58 -0300

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,9 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+// /public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "BAGyDSe4JGiUDkGYHBDO-c9IML0xqtn8K6WHq9kuL2I6Wz-ujRMxdZon-j3uPU0XINkiCeDnRGqYnPgcvW-RHwQ",
+  apiKey: "AIzaSyB3vZaQ41mSwiC8oG15WEj3T6KsGobZO9Q",
   authDomain: "meu-estoque-ab504.firebaseapp.com",
   projectId: "meu-estoque-ab504",
   storageBucket: "meu-estoque-ab504.firebasestorage.app",
