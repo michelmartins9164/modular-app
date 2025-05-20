a84c270e90646f819ddbdfd6f3532d0f3732d925
Michel Martins <michelmartins9164@gmail.com>
2025-05-20 19:45:36 -0300

    upodate

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import mkcert from 'vite-plugin-mkcert'
 import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/modular-app',
+  base: '/modular-app/',
   plugins: [react(), tsconfigPaths(),mkcert()],
     server: {
     https: true
